fa60d387063603c7a3f7469697f45bf399203e87
Mitra USYD <mitraahmadi129@gmail.com>
2024-08-08 21:17:27 +1000

    slowly adding in the tutor model into the data routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,11 @@
-const {User} = require('../models');
-const {signToken, AuthenticationError} = require('../utils/auth');
+const { User, Tutor } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if(context.user) {
-        return User.findOne({_id: context.user._id});
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
       }
       throw AuthenticationError;
     },
@@ -17,22 +17,22 @@ const resolvers = {
 
       return { token, user };
     },
-    login: async(parent, {email, password}) => {
-      const user = await User.findOne({email});
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-      if(!user) {
+      if (!user) {
         throw AuthenticationError;
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
-      if(!correctPw) {
+      if (!correctPw) {
         throw AuthenticationError;
       }
 
       const token = signToken(user);
 
-      return {token, user};
+      return { token, user };
     },
   },
 };
