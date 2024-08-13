c30d8e8ee7724015452ccc8482012acb068833f9
KiranRoss1999 <161179241+KiranRoss1999@users.noreply.github.com>
2024-08-13 21:41:09 +1000

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mkt-tutoring');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mtk-tutoring');
 
 module.exports = mongoose.connection;
